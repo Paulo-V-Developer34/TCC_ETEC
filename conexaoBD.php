f9e63f1e6f813404631924f2872eb28eb0b5262b
Paulo-V-Developer34 <165535750+Paulo-V-Developer34@users.noreply.github.com>
2024-11-21 00:54:12 -0300

    modify PDF css

--- conexaoBD.php
+++ conexaoBD.php
@@ -11,23 +11,24 @@ $usuario = $_ENV['DB_USER'];
 $senha = $_ENV['DB_PASS'];
 $bd = $_ENV['DB_NAME'];
 $port = $_ENV['DB_PORT'];
-$endpointId = $_ENV['DB_ENDPOINT'];
-$ssl_path = $_ENV['SSL_PATH'];
+// $endpointId = $_ENV['DB_ENDPOINT'];
+// $ssl_path = $_ENV['SSL_PATH'];
 
-$ssl_cert_path = __DIR__ . '/certs/ca.crt';
+// $ssl_cert_path = __DIR__ . './certs/isrgrootx1.pem';
 
 // Faz a conexão ao banco de dados usando PDF
 
 // // forma local
-// $conecta = new PDO("mysql:dbname=$bd; host=$host; port=$port; charset=utf8", $usuario, $senha);
+$conecta = new PDO("mysql:dbname=$bd; host=$host; port=$port; charset=utf8", $usuario, $senha);
 
 // forma de produção
 // Construir a string DSN
-// $dsn = "pgsql:host=$host;port=$port;dbname=$bd;options=--endpoint=$endpointId;sslmode=require";
-$dsn = "pgsql:host=$host;port=$port;dbname=$bd;options=--endpoint=$endpointId;sslmode=require;sslrootcert=$ssl_cert_path";
-// $dsn = "pgsql:host=$host;port=$port;dbname=$bd;sslmode=require";
+// $dsn = "pgsql:host=$host;port=$port;dbname=$bd;options=--endpoint=$endpointId;sslmode=require;sslrootcert=$ssl_cert_path";
+// $dsn = "pgsql:host=$host;port=$port;dbname=$bd;sslmode=require;sslrootcert=$ssl_cert_path";
+// $dsn = "pgsql:host=$host;port=$port;dbname=$bd;?options=endpoint%3D$endpointId;sslmode=require";
 
+// $newpassword = $senha . '$' . $endpointId;
 // Criar a conexão PDO
-$pdo = new PDO($dsn, $usuario, $senha);
+// $pdo = new PDO($dsn, $usuario, $newpassword);
 
 $conecta->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
